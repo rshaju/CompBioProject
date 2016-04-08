19956a9940e0fd45dbef447aa058928492b357b0
Yatri Desai <ydesai2@luc.edu>
2016-04-07 22:51:57 -0500

    Add Z
    Attempted to add Z to the tables. Converted the Ztable to a numpy array.

--- CellCounterParser.py
+++ CellCounterParser.py
@@ -15,9 +15,11 @@ def Ztable (zfile): #function to read in Z file
             string = string.split('\t')
             zslice = i
             cellcount = string[1]
+            cellcount = int(cellcount) #hopefully this works
             zlist.append(zslice)
             zlist.append(cellcount)
             Zlist.append(zlist)
+    Zlist = np.array(Zlist) #return as numpy array
     return Zlist
             
     
@@ -30,14 +32,49 @@ def ReadIntoTable(resultsfile): #function to read Restuls into table
     #Table: [0] = cell; [1] = area [2-7; don't want]
     #[8] = Xcoordinate [9] = Ycoordinate
     tableAXY = np.delete(table, np.s_[2:8], 1) #delete 2-7
-    print headerAXY
+    #print headerAXY
     return tableAXY
     #print tableAXY #table includes: cell, area, x, y
 
 ParsedTable = ReadIntoTable('Results.txt')
 z = Ztable ('ZtableResults.txt')
 print z
+
+##def InsertZ (ptable, ZTABLE):
+##    i = 0 #follow length of ztable
+##    j = 0 #follow parser table
+##    lengthZ = len(ZTABLE)
+
+
+##def insertZ (potato, zoo):
+##    i = 0 #follow length of zoo, and CS
+##    flag = 0 #keep up with the Parser table. and CC
+##    lengthZ = len(zoo) #final length of the z table
+##    temp_info_holder = [] #stores XY
+##    Pillow = [] #final list of updated info holding XYZ
+##    for num in zoo: #iterate through Z, get the cell counts per cross section
+##        while i < lengthZ: 
+##            item = zoo[i] #the element holding CS and CC
+##            item = item[1] #item has become the cell count
+##            if item > 0: #if there is a cell
+##                for order in range(item):
+##                    temp = (potato[flag])
+##                    temp.append(i + 1)
+##                    Pillow.append(temp)
+##                    #print(potato[flag])#.append(i) #the cell info
+##                    print(i + 1) #the CS
+##                    flag = flag + 1 #move up to next CC
+##            #print(item)
+##            i = i + 1
+##    
+##            
+##    return 4
+##
+##hi = insertZ (ParsedTable, z)
+
+#print z
 print ParsedTable
+##print hi
 
 
 ##def CompareCells(editedtable): #take in the edited table array
